da926a957e08cf1613a927201f8ad8ff3eb63ccc
Ayush Kumar <229260965+AyushKumar-06@users.noreply.github.com>
2026-06-05 21:24:17 +0530

    Completed bubble sort

--- Sorting.cpp
+++ Sorting.cpp
@@ -2,7 +2,7 @@
 using namespace std;
 
 //Selection sort
-
+/*
 #include <utility>
 int main(){
     int n;
@@ -37,3 +37,38 @@ int main(){
     
     return 0;
 }
+*/
+
+//Bubble sort
+/*
+void bubble_sort(int arr[],int n){
+    for(int i=n-1;i>=1;i--){
+        for(int j=0;j<i;j++){
+            if(arr[j]>arr[j+1]){
+                int temp = arr[j];
+                arr[j] = arr[j+1];
+                arr[j+1] = temp;
+            }
+        }
+    }
+}
+int main(){
+    int n;
+    cout << "Enter the number of elements in the array : ";
+    cin >> n;
+
+    int arr[n];
+    cout << "Enter the elements of the array : ";
+    for(int i=0;i<n;i++){
+        cin >> arr[i];
+    }
+    bubble_sort(arr,n);
+    //printing the sorted array
+    cout << "The sorted array is : ";
+    for(int i=0;i<n;i++){
+        cout << arr[i] << " ";
+    }
+
+    return 0;
+}
+*/
