ae1135f78df87a66e299ad0e4e698adaed3a534f
Ayush Kumar <229260965+AyushKumar-06@users.noreply.github.com>
2026-06-06 19:57:57 +0530

    Completed insertion sort

--- Sorting.cpp
+++ Sorting.cpp
@@ -39,17 +39,18 @@ int main(){
 }
 */
 
-//Bubble sort
-
+//Bubble sort (Optimised version)
+/*
 void bubble_sort(int arr[],int n){
     for(int i=n-1;i>=1;i--){
+    //Optimisation is the didswap part
         int didswap = 0;
         for(int j=0;j<i;j++){
             if(arr[j]>arr[j+1]){
                 int temp = arr[j];
                 arr[j] = arr[j+1];
                 arr[j+1] = temp;
-                didswap = 0;
+                didswap = 1;
             }
             if(didswap == 0){
                 break;
@@ -76,3 +77,39 @@ int main(){
 
     return 0;
 }
+*/
+
+//Insertion sort
+/*
+void insertion_sort(int arr[],int n){
+    int i,j;
+    for(i=0;i<=n-1;i++){
+        j=i;
+        while(j>0 && arr[j-1] > arr[j]){
+                int temp = arr[j];
+                arr[j] = arr[j-1];
+                arr[j-1] = temp;
+                j--; 
+        }
+    }
+}
+int main(){
+    int n;
+    cout << "Enter the number of elements in array : ";
+    cin >> n;
+
+    int arr[n];
+    cout << "Enter the elements of the array : ";
+    for(int i=0;i<n;i++){
+        cin >> arr[i];
+    }
+
+    insertion_sort(arr,n);
+
+    for(int i=0;i<n;i++){
+        cout << arr[i] << " ";
+    }
+
+    return 0;
+}
+*/
