0e2e0dce87a8b14a961d51407eb4eb691ce5d10e
Ayush Kumar <229260965+AyushKumar-06@users.noreply.github.com>
2026-07-11 23:06:41 +0530

    Completed Set matrix zeroes

--- Arrays_medium.cpp
+++ Arrays_medium.cpp
@@ -225,6 +225,48 @@ int secquence_LC(vector<int>& nums){
     }
     return longest;
 }
+
+//Set matrix zeroes
+void set_matrix(vector<vector<int>>& matrix){
+    int rows = matrix.size();
+    int columns = matrix[0].size();
+
+    int col0 = 1;
+
+    for(int i=0;i<rows;i++){
+        for(int j=0;j<columns;j++){
+            if(matrix[i][j] == 0){
+                matrix[i][0] = 0;
+            if(j!= 0){
+                matrix[0][j] = 0;
+            }
+            else{
+                col0 = 0;
+            }
+            }
+        }
+    }
+
+    for(int i=1;i<rows;i++){
+        for(int j=1;j<columns;j++){
+            if(matrix[i][0] == 0 && matrix[0][j] == 0){
+                 matrix[i][j] = 0;
+            }
+        }
+    }
+
+    if(matrix[0][0] == 0){
+        for(int j=0;j<columns;j++){
+            matrix[0][j] = 0;
+        }
+    }
+    
+    if(col0 == 0){
+        for(int i=0;i<rows;i++){
+            matrix[i][0] = 0;
+        }
+    }
+}
 int main(){
     int n;
     cout << "Enter the number of elements : ";
