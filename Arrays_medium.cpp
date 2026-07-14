9f8e9e73d9a8ec56b3945e32d7233648316b88bc
Ayush Kumar <229260965+AyushKumar-06@users.noreply.github.com>
2026-07-14 23:27:37 +0530

    Completed Majority element 2

--- Arrays_medium.cpp
+++ Arrays_medium.cpp
@@ -337,7 +337,65 @@ int subarray_count(vector<int>& nums){
     return count;
 }
 
+//Program to generate Pascal's Triangle
+vector<vector<int>> pascal_triangle(int numRows){
+    vector<vector<int>> ans(numRows);
 
+    for(int i=0;i<numRows;i++){
+        ans[i].resize(i+1,1);
+        for(int j=1;j<i;j++){
+            ans[i][j] = ans[i-1][j-1] + ans[i=1][j];
+        }
+    }
+    return ans;
+}
+
+//Majority element - 2
+vector<int> majority_2(vector<int>& nums){
+    vector<int> ans;
+    int num1 = nums[0];
+    int num2;
+    int cnt1 = 1;
+    int cnt2 = 0;
+    for(int i=1;i<nums.size();i++){
+        if(nums[i] == num1){
+            cnt1++;
+        }
+        else if(nums[i] == num2){
+            cnt2++;
+        }
+        else if(cnt1 == 0){
+            num1 = nums[i];
+            cnt1 = 1;
+        }
+        else if(cnt2 == 0){
+            num2 = nums[i];
+            cnt2 = 1;
+        }
+        else{
+            cnt1--;
+            cnt2--;
+        }
+    }
+    int cnt1_actual = 0;
+    int cnt2_actual = 0;
+    for(int i=0;i<nums.size();i++){
+        if(nums[i] == num1){
+            cnt1_actual++;
+        }
+        else if(nums[i] == num2){
+            cnt2_actual++;
+        }
+    }
+    int n = nums.size() / 3;
+    if(cnt1_actual > n){
+        ans.push_back(num1);
+    }
+    if(cnt2_actual > n){
+        ans.push_back(num2);
+    }
+    return ans;
+}
 
 int main(){
     int n;
