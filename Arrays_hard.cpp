ada1b01ec71b08302fcac22d4222391ed0bb1f79
Ayush Kumar <229260965+AyushKumar-06@users.noreply.github.com>
2026-08-07 14:04:33 +0530

    Completed Count inversions

--- Arrays_hard.cpp
+++ Arrays_hard.cpp
@@ -261,6 +261,64 @@ vector<int> elements_optimal(vector<int>& nums){
     return{(int)x,(int)y};
 }
 
+//Count inversions
+int count_reversal(vector<int>& nums){
+    int n = nums.size();
+    mergesort(nums,0,n-1);
+}
+
+int mergesort(vector<int>& nums,int low,int high){
+    int cnt = 0;
+    if(low >= high){
+        return cnt;
+    }
+
+    int mid = (low + high)/2;
+
+    //divide recursively the left half
+    cnt += mergesort(nums,low,mid);
+    //divide recursively the right half
+    cnt += mergesort(nums,mid,high);
+    //merge the 2 sorted halves
+    cnt += merge(nums,low,mid,high);
+
+    return cnt;
+}
+
+int merge(vector<int>& arr, int low, int mid, int high){
+    vector<int> temp;
+    int left = low;
+    int right = mid + 1;
+    int cnt = 0;
+
+    while(left <= mid && right <= high){
+        if(arr[left] <= arr[right]){
+            temp.push_back(arr[left]);
+            left++;
+        }
+
+        else{
+            temp.push_back(arr[right]);
+            right++;
+            cnt += mid - left + 1;
+        }
+    }
+
+    while(left <= mid){
+        temp.push_back(arr[left]);
+        left++;
+    }
+
+    while(right <= high){
+        temp.push_back(arr[right]);
+        right++;
+    }
+
+    for(int i=low;i<=high;i++){
+        arr[i] = temp[i - low];
+    }
+    return cnt;
+}
 
 
 int main(){
