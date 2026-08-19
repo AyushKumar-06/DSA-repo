05af81307707eaaaeea167ca2805322a5013092a
Ayush Kumar <229260965+AyushKumar-06@users.noreply.github.com>
2026-08-19 15:27:52 +0530

    Completed Count reverse pairs

--- Arrays_hard.cpp
+++ Arrays_hard.cpp
@@ -320,6 +320,74 @@ int merge(vector<int>& arr, int low, int mid, int high){
     return cnt;
 }
 
+//Count reverse pairs
+void merge1(vector<int>& arr,int low,int mid,int high){
+    vector<int> temp;       //temporary array to hold the sorted elements
+    int left = low;         
+    int right = mid+1;
+
+    while(left <= mid && right <= high){
+        if(arr[left] <= arr[right]){
+            temp.push_back(arr[left]);
+            left++;
+        }
+
+        else{
+            temp.push_back(arr[right]);
+            right++;
+        }
+    }
+
+    while(left <= mid){
+        temp.push_back(arr[left]);
+        left++;
+    }
+
+    while(right <= high){
+        temp.push_back(arr[right]);
+        right++;
+    }
+
+    for (int i = low; i <= high; i++) {
+        arr[i] = temp[i - low];
+    }
+}
+
+int countpairs(vector<int>& arr, int low, int mid, int high){
+    int right = mid + 1;
+    int cnt = 0;
+    for(int i=low;i<=mid;i++){
+        while(right<=high && arr[i] > 2LL * arr[right]) right++;
+        cnt += right - (mid + 1);
+    }
+    return cnt;
+}
+
+int mergesort(vector<int>& arr, int low,int high){
+    int cnt = 0;
+
+    if(low >= high){
+        return cnt;
+    }
+
+    int mid = (low+high)/2;
+
+    //applying merge sort to the left half
+    cnt += mergesort(arr,low,mid);
+    //applying merge sort to the right half
+    cnt += mergesort(arr,mid+1,high);
+    //Modification
+    cnt += countpairs(arr,low,mid,high);
+    //sorting and mergeing the 2 halves
+    merge1(arr,low,mid,high);
+
+    return cnt;
+}
+
+int team(vector <int> & skill, int n)
+{
+    return mergesort(skill, 0, n - 1);
+}
 
 int main(){
     int n;
