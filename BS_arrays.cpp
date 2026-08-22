a4e8977c6af403b13ea44e7004fed39aef298ea9
Ayush Kumar <229260965+AyushKumar-06@users.noreply.github.com>
2026-08-22 21:22:26 +0530

    Completed implementation of lower bound

---
+++ BS_arrays.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+using namespace std;
+
+//Binary search
+int binary_search(vector<int>& nums, int target){
+    int low = 0;
+    int high = nums.size() - 1;
+    int mid;
+
+    while(low <= high){
+        mid = low + (high - low)/2;
+
+        if(target == nums[mid]){
+            return mid;
+        }
+        else if(target > nums[mid]){
+            low = mid + 1;
+        }
+        else{
+            high = mid - 1;
+        }
+    }
+    
+    return -1;
+}
+
+//implement lower bound
+int lower_bound(vector<int>& nums, int target){
+    int low = 0;
+    int high = nums.size() - 1;
+    int mid;
+    int ans = nums.size();
+    while(low <= high){
+        mid = low + (high - low)/2;
+        if(nums[mid] >= target){
+            ans = mid;
+            high = mid - 1;
+        }
+        else{
+            low = mid + 1;
+        }
+    }
+    return ans;
+}
+
+
+int main(){
+
+}
